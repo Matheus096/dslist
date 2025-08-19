23eef2ceb0e3c585775baf4ca3052ca42feb9e94
Matheus Luiz <matheuslfdd096@gmail.com>
2025-08-19 01:15:26 -0300

    Criação do objeto GameDTO e implementação do método findById no GameService

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -15,9 +17,28 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long gameId) {
+        Game game = gameRepository.findById(gameId).get();
+        GameDTO dto = new GameDTO(game);
+        return dto;
+    }
+
+    /* 
+    Forma resumida de retornar o GameDTO diretamente sem a necessidade de criar uma variável GameDTO como feito antes acima:
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long gameId) {
+        Game game = gameRepository.findById(gameId).get();
+        return new GameDTO(game);
+    }
+
+    */
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
-        List<Game> result = gameRepository.findAll();
-        List<GameMinDTO> dto = result.stream().map(game -> new GameMinDTO(game)).toList();
+        List<Game> listGames = gameRepository.findAll();
+        List<GameMinDTO> dto = listGames.stream().map(game -> new GameMinDTO(game)).toList();
         return dto;
     }
 }
