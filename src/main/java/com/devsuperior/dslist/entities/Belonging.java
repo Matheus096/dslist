dde3748b0492bfdf40a8262b475931b8cf25b9db
Matheus Luiz <matheuslfdd096@gmail.com>
2025-08-18 23:54:44 -0300

    Seed de dados na tabela belonging para os jogos

--- src/main/java/com/devsuperior/dslist/entities/Belonging.java
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -24,7 +24,7 @@ public class Belonging {
 		this.position = position;
 	}
 
-	// Ao criar um novo objeto do tipo "Belonging", deve-se ter a referência do Game e do GameList, como pode se ver na imagem "modelo do projeto DSList.png"
+	// Ao criar um novo objeto do tipo "Belonging", deve-se ter a referência do Game e do GameList, como pode se ver na imagem "modelo do projeto DSList.png" e no print_20
 	// Por isso o construtor acima foi feito dessa forma, ja passando o Game e o GameList como parâmetros, e atribuindo diretamente eles ao objeto "id" do tipo BelongingPK
 
 	public void setGame(Game game) {
