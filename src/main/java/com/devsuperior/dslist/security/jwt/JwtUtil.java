22463e0c031e1a647f5c429c788dac35aa5fd8bd
Matheus Luiz <matheuslfdd096@gmail.com>
2025-10-18 00:55:12 -0300

    Criação da classe JwtUtil para geração e validação de tokens JWT e da classe JwtFilter para interceptar requisições HTTP e validar o token JWT presente no cabeçalho Authorization

---
+++ src/main/java/com/devsuperior/dslist/security/jwt/JwtUtil.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dslist.security.jwt;
+
+import java.util.Date;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtUtil {
+
+    private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        return extractUsername(token).equals(userDetails.getUsername());
+    }
+
+}
